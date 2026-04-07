da763047c2df142d0d208885369d64800a15cb79
phungh67 <phungh67@gmail.com>
2026-04-07 15:48:18 +0200

    feat: add lock for Pocket's methids

--- language-based-security/lab-1/lab1_start/backEnd/Pocket.java
+++ language-based-security/lab-1/lab1_start/backEnd/Pocket.java
@@ -1,9 +1,9 @@
 package backEnd;
 import java.io.File;
-import java.io.IOException;
 import java.io.RandomAccessFile;
-import java.io.FileReader;
-import java.io.BufferedReader;
+import java.nio.channels.FileLock;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 
 public class Pocket {
@@ -11,6 +11,7 @@ public class Pocket {
      * The RandomAccessFile of the pocket file
      */
     private RandomAccessFile file;
+    private Lock lock = new ReentrantLock(); // protect the Pocket when adding items
 
     /**
      * Creates a Pocket object
@@ -27,8 +28,16 @@ public class Pocket {
      * @param  product           product name to add to the pocket (e.g. "car")
      */
     public void addProduct(String product) throws Exception {
-        this.file.seek(this.file.length());
-        this.file.writeBytes(product+'\n'); 
+        lock.lock();
+        FileLock fileLock = null;
+        try {
+            fileLock = this.file.getChannel().lock();
+            this.file.seek(this.file.length());
+            this.file.writeBytes(product+'\n');
+        } finally {
+            if (fileLock != null) fileLock.release();
+            lock.unlock();
+        }
     }
 
     /**
