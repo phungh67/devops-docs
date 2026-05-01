a89f98df2a1afff07e32dab551cff232ffe9dfc9
phungh67 <phungh67@gmail.com>
2026-05-01 19:47:25 +0200

    feat: first sketch with module 1 - input filter

---
+++ language-based-security/project/main.py
@@ -0,0 +1,117 @@
+import socket
+import os
+import json
+import html
+
+from classes.lexical_extractor import LexicalExtractor
+from classes.regex_filter import RegexFilter
+from classes.vector_db import VectorDatabase
+
+from modules.input_guard import words_matching_simple
+from modules.vector_matching import (
+    construct_vocabulary,
+    construct_word_indexes,
+    vectorized_input_database,
+    vectorized_sentence,
+    sentences
+)
+
+SOCKET_PATH = "/tmp/prompt_injection_guard.sock"
+
+class PromptInjectionGuardDaemon:
+    """
+    Class definition for the Prompt Injection Guard Daemon
+    """
+    def __init__(self):
+        self.extractor = LexicalExtractor()
+        self.regex_filter = RegexFilter()
+
+        self.vocabulary = construct_vocabulary(sentences)
+        self.word_index = construct_word_indexes(self.vocabulary)
+        sentence_vectors = vectorized_input_database(sentences, self.word_index, self.vocabulary)
+
+        self.vector_db = VectorDatabase()
+        for sentence, vector in sentence_vectors.items():
+            self.vector_db.add_vector(sentence, vector)
+
+        self.DROP_SCORE = 0.80
+        self.QUARANTINE_SCORE = 0.45
+
+    def frame_data(self, data: str) -> str:
+        """Santitizes and wraps text extracted in XML tags."""
+        if not data:
+            return ""
+        
+        escaped_data = html.escape(data, quote=True)
+        return f"\n<user_context>\n{escaped_data}\n</user_context>"
+
+    def process_input(self, raw_input: str) -> dict:
+        """Detect, extract and sanitize user's input"""
+
+        if words_matching_simple(raw_input) == 0:
+            return {"status": "BLOCKED", "reason": "Failed Stage 1: Naive string match."}
+
+        if not self.regex_filter.scan(raw_input):
+            return {"status": "BLOCKED", "reason": "Failed Stage 2: Obfuscation/Structural match."}
+
+        query_vector = vectorized_sentence(raw_input, self.word_index, self.vocabulary)
+        similar_sentences = self.vector_db.find_similar_vectors(query_vector, num_results=2)
+        highest_similarity = similar_sentences[0][1] if similar_sentences else 0.0
+
+        print(f"[INFO] Vector similarity detection Score: {highest_similarity:.4f}")
+
+        if highest_similarity >= self.DROP_SCORE:
+            return {"status": "BLOCKED", "reason": "Failed Stage 3: Semantic similarity to known attacks."}
+
+
+        extracted = self.extractor.extract(raw_input)
+        intent = extracted.get("intentions", "")
+        data = extracted.get("data", None)
+
+        safe_xml_data = self.frame_data(data)
+        final_payload = f"{intent}{safe_xml_data}"
+
+        return {
+            "status": "APPROVED",
+            "safe_payload": final_payload
+        }
+
+    def start(self):
+        """Start method of the class"""
+        if os.path.exists(SOCKET_PATH):
+            os.remove(SOCKET_PATH)
+
+        server = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+        server.bind(SOCKET_PATH)
+        server.listen(5)
+
+        print(f"\n[DAEMON] Module 1 active. Listening on {SOCKET_PATH}...")
+
+        try:
+            while True:
+                conn, addr = server.accept()
+                raw_data = conn.recv(8192).decode('utf-8')
+
+                if not raw_data:
+                    conn.close()
+                    continue
+            
+                print("-" * 60)
+                print(f"[REQUEST IN] {raw_data[:50]}...")
+
+                result = self.process_input(raw_data)
+
+                conn.sendall(json.dumps(result).encode('utf-8'))
+                conn.close()
+
+                if result["status"] == "APPROVED":
+                    print("[REQUEST OUT] Payload is sanitized and ready for Ollama.")
+        except KeyboardInterrupt:
+            print("\n[DAEMON] Shutting down...")
+        finally:
+            if os.path.exists(SOCKET_PATH):
+                os.remove(SOCKET_PATH)   
+
+if __name__ == "__main__":
+    daemon = PromptInjectionGuardDaemon()
+    daemon.start()        
